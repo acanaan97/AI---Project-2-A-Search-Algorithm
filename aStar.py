3afa690b0357351566038313425813654368aaaa
acanaan97 <44300613+acanaan97@users.noreply.github.com>
2020-01-28 16:21:52 -0700

    Update aStar.py

--- aStar.py
+++ aStar.py
@@ -2,3 +2,10 @@ import filehandler as filehandler
 import helpers as helpers
 import math
 
+# for each node in locations, add a third element to the pair for estimate score
+Open = []
+Closed = []
+
+filehandler.init()
+temp = helpers.getInput()
+print(temp[3])
