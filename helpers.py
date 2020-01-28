1f17764951c3db945ece4e64d5ae2cdb5b0c05fb
acanaan97 <44300613+acanaan97@users.noreply.github.com>
2020-01-28 16:22:42 -0700

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,42 @@
 import math
+import filehandler as filehandler
 
 def Square(x1, x2, y1, y2):
     res = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     return res
+
+def getInput():
+    blacklist = []
+    flag = False
+    while(flag == False):
+        inputstr = input("Name the starting city: ")
+        if inputstr in filehandler.Locations.keys():
+            flag = True
+        else:
+            print("Invalid city. Try again below.")
+
+    start = inputstr
+
+    flag = False
+    while(flag == False):
+        inputstr = input("Name the ending city: ")
+        if inputstr in filehandler.Locations.keys():
+            flag = True
+        else:
+            print("Invalid city. Try again below.")
+
+    end = inputstr
+
+    flag = False
+    while(flag == False):
+        exclusion = input("Name cities to exclude in a comma-separated list (no spaces): ")
+        exclusions = exclusion.split(",")
+        for x in exclusions:
+            if x in filehandler.Locations.keys():
+                blacklist.append(x)
+                flag = True
+            else:
+                print("Invalid city. Try again below.")
+
+
+    return start,end, blacklist
